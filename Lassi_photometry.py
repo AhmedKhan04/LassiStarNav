6a4ee1a6da8ed23be7e9d9ad9d9cf840b1dd9946
Ahmed Khan <ahmedk2@illinois.edu>
2025-09-24 19:55:48 -0500

    added in original

---
+++ Lassi_photometry.py
@@ -0,0 +1,12 @@
+
+# this is the photometry pipeline 
+
+import numpy as np 
+import scipy as sp 
+import matplotlib.pyplot as plt
+from photutils import aperture
+import astropy as ap 
+from PIL import Image
+
+
+
