70c3639ab8b4b064c90cdb26f269c25af66b24bd
Ahmed Khan <ahmedk2@illinois.edu>
2025-10-11 20:18:39 -0500

    What in the world even is a centroid????

--- light_curve_extraction.py
+++ light_curve_extraction.py
@@ -15,14 +15,15 @@ import os
 #import cv2
 
 
+
 def get_max(img): 
     maxVal = np.amax(img)
-    print('--------------')
-    print(maxVal)
+    #print('--------------')
+    #print(maxVal)
     maxLoc = np.unravel_index(np.argmax(img), img.shape)
     maxLoc = [(maxLoc[1], maxLoc[0])]
-    print(maxLoc)
-    print('--------------')
+    #print(maxLoc)
+    #print('--------------')
     return maxLoc
 
 # i am going to calibrate my picture here since we do not have a proper class structure yet....
@@ -38,6 +39,7 @@ calibrated = dark_corrected / flat_norm
 
 folder_path = r"images\WASP-12b_example_uncalibrated_images\uncalibrated"
 initial = True 
+
 for filename in os.listdir(folder_path):
     #print(filename)
     file_path = os.path.join(folder_path, filename)
@@ -101,12 +103,12 @@ for filename in os.listdir(folder_path):
     cords = np.array([(3499.825554241658, 39.37473823979557)])  # coordinates from image A
     transform, (src_list, ref_list) = aa.find_transform(calibrated_second, calibrated)
 
-    print(transform)  # see what transform was found
+    #print(transform)  # see what transform was found
 
     cords_transformed = transform(cords)
 
-    print("Original coords:", cords)
-    print("Transformed coords:", cords_transformed)
+    #print("Original coords:", cords)
+    #print("Transformed coords:", cords_transformed)
 
 
     # override for now for inspection
@@ -122,8 +124,8 @@ for filename in os.listdir(folder_path):
     x, y = t_p[0]
     x, y = int(x), int(y)
     half_box = 50
-    print(x)
-    print(y)
+    #print(x)
+    #print(y)
     x1, x2 = max(0, x - half_box), min(nx, x + half_box)
     y1, y2 = max(0, y - half_box), min(ny, y + half_box)
 
@@ -142,11 +144,70 @@ for filename in os.listdir(folder_path):
         print("No stars found, using previous coordinates") 
         cords_transformed = past_cord
     else:
-        cords_transformed = get_max(masked_data)
-    print(f"refinied cords {cords_transformed}")
+        cords_transformed = get_max(masked_data) 
+        increment = 5 # degrees to break up
+        radius = 1 #radius between centers 
+        degrees = np.arange(360/increment) * increment
+        radians = np.radians(degrees)
+        direction_matrix = np.array([np.cos(radians), np.sin(radians)]).T
+        direction_matrix *= radius
+        #plt.figure()
+        
+        #plt.imshow(calibrated_second, cmap='grey', origin='lower', vmin=median_s-2*std_s, vmax=median_s+5*std_s)
+        
+        aper_t = CircularAperture(cords_transformed[0], r=ap_radius)
+        aper_t.plot(color='green')
+        #plt.scatter(cords_transformed[0][0], cords_transformed[0][1])
+        phot_table = aperture_photometry(calibrated_second, aper_t)
+        
+        best_mean = phot_table['aperture_sum'][0] / (np.pi * ap_radius**2)
+        best_aperture = aper_t
+        best_vec = cords_transformed[0]
+        best_achieved = False
+        best_previous = cords_transformed[0]
+        while(best_achieved == False):
+            for vec in direction_matrix: 
+                #print('NEXT CORD')
+                test_coord = (cords_transformed + np.flip(vec))[0]
+
+                #print(test_coord)
+                aper_t = CircularAperture(test_coord, r=ap_radius)
+                #aper_t.plot(color='green')
+                #plt.scatter(test_coord[0], test_coord[1])
+                phot_table = aperture_photometry(calibrated_second, aper_t)
+                mean_flux = phot_table['aperture_sum'][0] / (np.pi * ap_radius**2)
+                if mean_flux > best_mean:
+                    best_mean = mean_flux
+                    best_aperture = aper_t
+                    best_vec = vec
+            if(np.array_equal(best_previous, best_vec)):
+                best_achieved = True
+            else: 
+                best_previous = best_vec
+                cords_transformed = cords_transformed + np.flip(best_vec)
+                #print(f"New best mean flux: {best_mean}")
+                #print(f"New best vector offset: {best_vec}")
+                #print(f"New best transformed coords: {(cords_transformed + np.flip(best_vec))[0]}")
+        
+        if best_aperture is not None:
+            best_aperture.plot(color='red', lw=2)  # highlight the best one
+            #plt.scatter((cords_transformed + np.flip(best_vec))[0][0], (cords_transformed + np.flip(best_vec))[0][1], color='red', s=60, marker='x')
+            print(f"Best aperture mean flux: {best_mean}")
+            print(f"Best vector offset: {best_vec}")
+            print(f"Best transformed coords: {(cords_transformed + np.flip(best_vec))[0]}") 
+        
+        #print(degrees)
+        #print(direction_matrix) 
+
     
+    #print(f"refinied cords {cords_transformed}")
+    #plt.scatter((cords_transformed)[0][0], (cords_transformed)[0][1]) # add in our initial centroid. 
+    
+    #plt.show()
+    #plt.close()
     #cords_transformed = DAOStarFinder(fwhm=FWHM, threshold=5.*std, xycoords=cords)
     #cords_transformed = np.array([(3499.825554241658, 39.37473823979557)]) 
+
     aper_t = CircularAperture(cords_transformed[0], r=ap_radius)
     ann_t = CircularAnnulus(cords_transformed[0], r_in=ann_inner, r_out=ann_inner+ann_width)
 
@@ -170,9 +231,9 @@ for filename in os.listdir(folder_path):
 
     fig, ax = plt.subplots(figsize = (7,7))
     plt.imshow(calibrated_second, cmap='grey', origin='lower', vmin=median_s-2*std_s, vmax=median_s+5*std_s)
-    aper_t.plot(color='green')
+    aper_t.plot(color='red')
     ann_t.plot(color='cyan')
-    print(aper_t)
+    #print(aper_t)
 
     xmin_pixel = -50 + cords_transformed[0][0]
     xmax_pixel = 50 + cords_transformed[0][0]
@@ -191,12 +252,14 @@ for filename in os.listdir(folder_path):
     
     ax.set_xlim(xmin_pixel, xmax_pixel)
     ax.set_ylim(ymin_pixel, ymax_pixel)
-    print(xmin_pixel, xmax_pixel, ymin_pixel, ymax_pixel) 
+    #print(xmin_pixel, xmax_pixel, ymin_pixel, ymax_pixel) 
     #plt.scatter(sources['xcentroid'], sources['ycentroid'], s=40, facecolors='none', edgecolors='r', label = "DOA")
     #plt.scatter(sources_2['xcentroid'], sources_2['ycentroid'], s=40, facecolors='none', edgecolors='b', label = "IRAF")
     #plt.legend(loc = "upper left")
+    
     plt.title("Calibrated Image with photometry")
     plt.xlabel("X (pixels)")
     plt.ylabel("Y (pixels)")
     plt.savefig(fr"images\aperture_masks\{filename}.png")
     plt.close()
+ 
